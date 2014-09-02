b3d3369a282bae8c08b41b4e2f6b135033e5153e
andrroy <andreas@royrvik.org>
2014-09-02 21:07:01 +0200

    Initial Commit

---
+++ wikigameTests/wikigameTests.m
@@ -0,0 +1,34 @@
+//
+//  wikigameTests.m
+//  wikigameTests
+//
+//  Created by Andreas Røyrvik on 02.09.14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface wikigameTests : XCTestCase
+
+@end
+
+@implementation wikigameTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
