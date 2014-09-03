b495ed4829e8ec1923d14ea2c40f22d8115cebec
andrroy <andreas@royrvik.org>
2014-09-03 20:08:53 +0200

    Added HomeScreen View

---
+++ HomeScreenViewController.h
@@ -0,0 +1,14 @@
+//
+//  HomeScreenViewController.h
+//  wikigame
+//
+//  Created by Andreas Røyrvik on 03.09.14.
+//  Copyright (c) 2014 Andreas Røyrvik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIView *homeScreen;
+
+@end
